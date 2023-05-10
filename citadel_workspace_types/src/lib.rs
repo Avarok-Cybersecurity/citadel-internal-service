93c94f43a14c2dab63498476a06e9bbbfccec660
Thomas Braun <braunth@oregonstate.edu>
2023-05-10 11:55:27 -0400

    init workspace

---
+++ citadel_workspace_types/src/lib.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub enum InternalServicePayload {
+    Connect {},
+    Register {},
+    Message {},
+    Disconnect {},
+    SendFile {},
+    DownloadFile {},
+}
