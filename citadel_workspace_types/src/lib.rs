4a9db3970324ed965518f7247d7e8263acbb6b83
Tjemmmic <donovan.tjemmes@tsaremedia.com>
2023-05-21 18:01:01 -0500

    CitadelWorkspaceService lib enum update

--- citadel_workspace_types/src/lib.rs
+++ citadel_workspace_types/src/lib.rs
@@ -1,15 +1,22 @@
 use bytes::BytesMut;
-use citadel_sdk::prelude::{SecBuffer, SecurityLevel};
+use citadel_sdk::prelude::{ConnectMode, ObjectSource, SecBuffer, SecureProtocolPacket, SecurityLevel, SessionSecuritySettings, Ticket, TransferType, UdpMode, UserIdentifier};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use uuid::Uuid;
+use std::path::PathBuf;
+use std::time::Duration;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum InternalServicePayload {
     Connect {
         uuid: Uuid,
+        server_addr: SocketAddr,
         username: String,
         password: SecBuffer,
+        connect_mode: ConnectMode,
+        udp_mode: UdpMode,
+        keep_alive_timeout: Option<Duration>,
+        session_security_settings: SessionSecuritySettings
     },
     ConnectSuccess {
         cid: u64,
@@ -23,6 +30,7 @@ pub enum InternalServicePayload {
         full_name: String,
         username: String,
         proposed_password: SecBuffer,
+        default_security_settings: SessionSecuritySettings
     },
     RegisterSuccess {
         id: Uuid,
@@ -31,18 +39,41 @@ pub enum InternalServicePayload {
         message: String,
     },
     Message {
-        message: Vec<u8>,
+        message: SecureProtocolPacket,
         cid: u64,
         security_level: SecurityLevel,
     },
     MessageReceived {
-        message: BytesMut,
+        message: SecureProtocolPacket,
         cid: u64,
         peer_cid: u64,
+        security_level: SecurityLevel
     },
-    Disconnect {},
-    SendFile {},
-    DownloadFile {},
+    Disconnect {
+        implicated_cid: u64,             // Everything other than the implicated_cid would be used to craft the disconnect PeerSignal
+        ticket: Ticket,
+        cid_opt: Option<u64>,
+        success: bool,
+        v_conn_type: Option<VirtualConnectionType>,
+        message: String
+    },
+    SendFile {
+        source: dyn ObjectSource,
+        cid: u64,
+        transfer_security_level: SecurityLevel,
+        chunk_size: usize,
+        transfer_type: TransferType
+    },
+    DownloadFile {
+        virtual_path: dyn Into<PathBuf>,
+        transfer_security_level: SecurityLevel,
+        delete_on_pull: bool
+    },
+
+    StartGroup {
+        initial_users_to_invite: Option <Vec<UserIdentifier>>,
+        session_security_settings: SessionSecuritySettings
+    },
 
     ServiceConnectionAccepted {
         id: Uuid,
