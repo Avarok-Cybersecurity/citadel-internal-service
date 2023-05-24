2cd9f14e97062a3c1bea5390639b82f766c8c806
Donovan <37707055+Tjemmmic@users.noreply.github.com>
2023-05-24 17:39:54 -0500

    Update lib.rs
    Updates and fixes from live coding meeting

--- citadel_workspace_types/src/lib.rs
+++ citadel_workspace_types/src/lib.rs
@@ -1,8 +1,10 @@
-use bytes::BytesMut;
-use citadel_sdk::prelude::{SecBuffer, SecurityLevel};
+use bytes::{Bytes, BytesMut};
+use citadel_sdk::prelude::{ConnectMode, SecBuffer, SecurityLevel, SessionSecuritySettings, TransferType, UdpMode, UserIdentifier};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use uuid::Uuid;
+use std::path::PathBuf;
+use std::time::Duration;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum InternalServiceResponse {
@@ -21,13 +23,23 @@ pub enum InternalServiceResponse {
     ServiceConnectionAccepted {
         id: Uuid,
     },
+    MessageSent {
+        cid: u64,
+        // TODO: investigate passing a message hash or a trace id
+    },
+    MessageSendError {
+        cid: u64,
+        message: String,
+    },
     MessageReceived {
         message: BytesMut,
         cid: u64,
         peer_cid: u64,
     },
-    DisconnectSuccess(u64),
-    DisconnectFailed(u64),
+    DisconnectSuccess { cid: u64 },
+    DisconnectFailure { cid: u64, message: String },
+    SendFileSuccess { cid: u64 },
+    SendFileFailure { cid: u64, message: String },
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -36,6 +48,10 @@ pub enum InternalServicePayload {
         uuid: Uuid,
         username: String,
         password: SecBuffer,
+        connect_mode: ConnectMode,
+        udp_mode: UdpMode,
+        keep_alive_timeout: Option<Duration>,
+        session_security_settings: SessionSecuritySettings
     },
     Register {
         uuid: Uuid,
@@ -43,9 +59,11 @@ pub enum InternalServicePayload {
         full_name: String,
         username: String,
         proposed_password: SecBuffer,
+        default_security_settings: SessionSecuritySettings
     },
     Message {
-        message: Vec<u8>,
+        uuid: Uuid,
+        message: Bytes,
         cid: u64,
         user_cid: u64,
         security_level: SecurityLevel,
@@ -54,6 +72,23 @@ pub enum InternalServicePayload {
         uuid: Uuid,
         cid: u64,
     },
-    SendFile {},
-    DownloadFile {},
+    SendFile {
+        uuid: Uuid,
+        source: PathBuf,
+        cid: u64,
+        chunk_size: usize,
+        transfer_type: TransferType
+    },
+    DownloadFile {
+        virtual_path: PathBuf,
+        transfer_security_level: SecurityLevel,
+        delete_on_pull: bool,
+        cid: u64,
+        uuid: Uuid
+    },
+    StartGroup {
+        initial_users_to_invite: Option<Vec<UserIdentifier>>,
+        cid: u64,
+        uuid: Uuid
+    },
 }
