3958f814c99c2bc21baf388e6defd9cbc66da6da
Tjemmmic <michaeltdonovan@yahoo.com>
2023-12-13 17:35:57 -0600

    Group Kick Test

--- citadel_workspace_service/tests/group_chat.rs
+++ citadel_workspace_service/tests/group_chat.rs
@@ -6,11 +6,11 @@ mod tests {
     use citadel_logging::info;
     use citadel_sdk::prelude::{MemberState, UserIdentifier};
     use citadel_workspace_types::{
-        GroupCreateSuccess, GroupEndSuccess, GroupEnded, GroupInvitation, GroupInviteSuccess,
-        GroupJoinRequestReceived, GroupLeaveSuccess, GroupLeft, GroupListGroupsForSuccess,
-        GroupMemberStateChanged, GroupRequestJoinFailure, GroupRequestJoinSuccess,
-        GroupRespondRequestFailure, GroupRespondRequestSuccess, InternalServiceRequest,
-        InternalServiceResponse,
+        GroupCreateSuccess, GroupDisconnected, GroupEndSuccess, GroupEnded, GroupInvitation,
+        GroupInviteSuccess, GroupJoinRequestReceived, GroupKickFailure, GroupKickSuccess,
+        GroupLeaveSuccess, GroupLeft, GroupListGroupsForSuccess, GroupMemberStateChanged,
+        GroupRequestJoinFailure, GroupRequestJoinSuccess, GroupRespondRequestFailure,
+        GroupRespondRequestSuccess, InternalServiceRequest, InternalServiceResponse,
     };
     use std::error::Error;
     use std::net::SocketAddr;
@@ -590,6 +590,197 @@ mod tests {
                 }
             } else {
                 info!(target: "citadel", "Service A Waiting For GroupEnded Confirmation - Received {service_a_inbound:?}");
+            }
+        } else {
+            panic! {"Group Creation Error: Service A did not receive success response"};
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_citadel_workspace_service_group_kick() -> Result<(), Box<dyn Error>> {
+        citadel_logging::setup_log();
+        // internal service for peer A
+        let bind_address_internal_service_a: SocketAddr = "127.0.0.1:55536".parse().unwrap();
+        // internal service for peer B
+        let bind_address_internal_service_b: SocketAddr = "127.0.0.1:55537".parse().unwrap();
+        // internal service for peer C
+        let bind_address_internal_service_c: SocketAddr = "127.0.0.1:55538".parse().unwrap();
+
+        let mut peer_return_handle_vec = register_and_connect_to_server_then_peers(vec![
+            bind_address_internal_service_a,
+            bind_address_internal_service_b,
+            bind_address_internal_service_c,
+        ])
+        .await?;
+
+        let (peer_one, peer_two) = peer_return_handle_vec
+            .as_mut_slice()
+            .split_at_mut(1 as usize);
+        let (peer_two, peer_three) = peer_two.split_at_mut(1 as usize);
+        let (to_service_a, from_service_a, cid_a) = peer_one.get_mut(0 as usize).unwrap();
+        let (to_service_b, from_service_b, cid_b) = peer_two.get_mut(0 as usize).unwrap();
+        let (to_service_c, from_service_c, cid_c) = peer_three.get_mut(0 as usize).unwrap();
+
+        let mut initial_users_to_invite: Vec<UserIdentifier> = Vec::new();
+        initial_users_to_invite.push(UserIdentifier::from(*cid_b));
+        initial_users_to_invite.push(UserIdentifier::from(*cid_c));
+        let send_group_create_payload = InternalServiceRequest::GroupCreate {
+            cid: *cid_a,
+            request_id: Uuid::new_v4(),
+            initial_users_to_invite: Some(initial_users_to_invite),
+        };
+        to_service_a.send(send_group_create_payload).unwrap();
+
+        let deserialized_service_a_payload_response = from_service_a.recv().await.unwrap();
+
+        if let InternalServiceResponse::GroupCreateSuccess(GroupCreateSuccess {
+            cid: _,
+            group_key,
+            request_id: _,
+        }) = &deserialized_service_a_payload_response
+        {
+            // Service B Accepts Invitation
+            let service_b_group_create_invite = from_service_b.recv().await.unwrap();
+            if let InternalServiceResponse::GroupInvitation(..) = &service_b_group_create_invite {
+                let group_invite_response = InternalServiceRequest::GroupRespondRequest {
+                    cid: *cid_b,
+                    peer_cid: *cid_a,
+                    group_key: *group_key,
+                    response: true,
+                    request_id: Uuid::new_v4(),
+                    invitation: true,
+                };
+                to_service_b.send(group_invite_response).unwrap();
+                let deserialized_service_b_payload_response = from_service_b.recv().await.unwrap();
+                if let InternalServiceResponse::GroupRespondRequestSuccess(..) =
+                    &deserialized_service_b_payload_response
+                {
+                    info!(target: "citadel","Service B Accepted Group Invite");
+                } else if let InternalServiceResponse::GroupRespondRequestFailure(..) =
+                    &deserialized_service_b_payload_response
+                {
+                    panic!("Service B Failed Upon Responding to Group Invite");
+                }
+            } else {
+                panic!("Service B Invitation Not Received");
+            }
+
+            // Service C Accepts Group Invitation
+            let service_c_group_create_invite = from_service_c.recv().await.unwrap();
+            if let InternalServiceResponse::GroupInvitation(..) = &service_c_group_create_invite {
+                let group_invite_response = InternalServiceRequest::GroupRespondRequest {
+                    cid: *cid_c,
+                    peer_cid: *cid_a,
+                    group_key: *group_key,
+                    response: true,
+                    request_id: Uuid::new_v4(),
+                    invitation: true,
+                };
+                to_service_c.send(group_invite_response).unwrap();
+                let deserialized_service_c_payload_response = from_service_c.recv().await.unwrap();
+                if let InternalServiceResponse::GroupRespondRequestSuccess(..) =
+                    &deserialized_service_c_payload_response
+                {
+                    info!(target: "citadel","Service C Accepted Group Invite");
+                } else if let InternalServiceResponse::GroupRespondRequestFailure(..) =
+                    &deserialized_service_c_payload_response
+                {
+                    panic!("Service C Failed Upon Responding to Group Invite");
+                }
+            } else {
+                panic!("Service C Invitation Not Received");
+            }
+
+            // Service A Kicks the other group members
+            let service_a_outbound = InternalServiceRequest::GroupKick {
+                cid: *cid_a,
+                peer_cid: *cid_b,
+                group_key: *group_key,
+                request_id: Uuid::new_v4(),
+            };
+            to_service_a.send(service_a_outbound).unwrap();
+
+            let _ = from_service_a.recv().await.unwrap(); // Receive unnecessary MemberStateChanged
+            let _ = from_service_a.recv().await.unwrap(); // responses from Service B and C joining
+            let service_a_inbound = from_service_a.recv().await.unwrap();
+            if let InternalServiceResponse::GroupKickSuccess(GroupKickSuccess {
+                cid,
+                group_key: kick_group,
+                request_id: _,
+            }) = &service_a_inbound
+            {
+                assert_eq!(cid, cid_a);
+                assert_eq!(kick_group, group_key);
+                info!(target: "citadel", "Service B was successfully kicked from the group {kick_group:?}");
+            } else if let InternalServiceResponse::GroupKickFailure(GroupKickFailure {
+                cid: _,
+                message,
+                request_id: _,
+            }) = &service_a_inbound
+            {
+                panic!("Group Kick Error: Service B could not be kicked - {message:?}");
+            } else {
+                panic!("Group Kick Error: Received Unexpected Response {service_a_inbound:?}");
+            }
+
+            let service_a_outbound = InternalServiceRequest::GroupKick {
+                cid: *cid_a,
+                peer_cid: *cid_c,
+                group_key: *group_key,
+                request_id: Uuid::new_v4(),
+            };
+            to_service_a.send(service_a_outbound).unwrap();
+
+            let service_a_inbound = from_service_a.recv().await.unwrap();
+            if let InternalServiceResponse::GroupKickSuccess(GroupKickSuccess {
+                cid,
+                group_key: kick_group,
+                request_id: _,
+            }) = &service_a_inbound
+            {
+                assert_eq!(cid, cid_a);
+                assert_eq!(kick_group, group_key);
+                info!(target: "citadel", "Service C was successfully kicked from the group {kick_group:?}");
+            } else if let InternalServiceResponse::GroupKickFailure(GroupKickFailure {
+                cid: _,
+                message,
+                request_id: _,
+            }) = &service_a_inbound
+            {
+                panic!("Group Kick Error: Service C could not be kicked - {message:?}");
+            } else {
+                panic!("Group Kick Error: Received Unexpected Response {service_a_inbound:?}");
+            }
+
+            // Service B is notified that it was kicked
+            let _ = from_service_b.recv().await.unwrap(); // MemberStateChanged from Service C Joining
+            let service_b_inbound = from_service_b.recv().await.unwrap();
+            if let InternalServiceResponse::GroupDisconnected(GroupDisconnected {
+                cid: _,
+                group_key: disconnected_group,
+                request_id: _,
+            }) = &service_b_inbound
+            {
+                assert_eq!(group_key, disconnected_group);
+            } else {
+                panic! {"Service B did not received expected kick notification - instead received {service_b_inbound:?}"};
+            }
+
+            // Service C is notified that it was kicked
+            let _ = from_service_c.recv().await.unwrap(); // MemberStateChanged from Service B getting kicked
+            let _ = from_service_c.recv().await.unwrap(); // Extra MemberStateChanged Not Needed here
+            let service_c_inbound = from_service_c.recv().await.unwrap();
+            if let InternalServiceResponse::GroupDisconnected(GroupDisconnected {
+                cid: _,
+                group_key: disconnected_group,
+                request_id: _,
+            }) = &service_c_inbound
+            {
+                assert_eq!(group_key, disconnected_group);
+            } else {
+                panic! {"Service C did not received expected kick notification - instead received {service_c_inbound:?}"};
             }
         } else {
             panic! {"Group Creation Error: Service A did not receive success response"};
